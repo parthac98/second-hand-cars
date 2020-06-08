ea7585cc1e282895b7368b41b96bfb05576c4be8
Partha Das <parthadas@parthas-air.home>
2020-06-08 07:10:58 +0200

    created service POST to get the data from the RestAPI and tested

--- OldCar/src/app/app.component.ts
+++ OldCar/src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PostService } from './post.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+selector: 'app-root',
+templateUrl: './app.component.html',
+styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'OldCar';
+export class AppComponent implements OnInit {
+// variable to hold API responce data
+carDataAllInOne;
+
+// constructor
+constructor(private service:PostService) {}
+
+  // OnInIt function to call the API to get the data
+  ngOnInit() {
+      this.service.getPosts()
+        .subscribe(response => {
+          // Using 'dataRestructureing' function to add fav and add to cart value to the responce JSON
+          this.carDataAllInOne = this.dataRestructureing(response);
+        });
+  }
+
+  // function to add fav and add to cart value to the responce JSON
+  dataRestructureing(response){
+    for (let objs of response) {
+      for (let car of objs.cars.vehicles) {
+        car["fav"] = 0;
+        car["addedToCart"] = 0;
+      }
+    }
+
+    return response;
+  }
 }
