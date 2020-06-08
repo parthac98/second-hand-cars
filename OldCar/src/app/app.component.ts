146528e5d58dfce34b869ae4b10c746714069f19
Partha Das <parthadas@parthas-air.home>
2020-06-08 08:22:49 +0200

    data showing in front end, fab button and add to cart buton logic done

--- OldCar/src/app/app.component.ts
+++ OldCar/src/app/app.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from './post.service';
-
+   
 @Component({
-selector: 'app-root',
-templateUrl: './app.component.html',
-styleUrls: ['./app.component.css']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-// variable to hold API responce data
-carDataAllInOne;
-
-// constructor
-constructor(private service:PostService) {}
-
+  // variable to hold API responce data
+  carDataAllInOne;
+  
+  // constructor
+  constructor(private service:PostService) {}
+  
   // OnInIt function to call the API to get the data
   ngOnInit() {
       this.service.getPosts()
@@ -21,16 +21,38 @@ constructor(private service:PostService) {}
           this.carDataAllInOne = this.dataRestructureing(response);
         });
   }
-
+  
   // function to add fav and add to cart value to the responce JSON
   dataRestructureing(response){
     for (let objs of response) {
       for (let car of objs.cars.vehicles) {
-        car["fav"] = 0;
-        car["addedToCart"] = 0;
-      }
+        car["fav"] = false;
+        car["addedToCart"] = false;
+      } 
     }
 
     return response;
   }
+
+  // function on click to FAV
+  addToFab($event, carId){    
+    for (let objs of this.carDataAllInOne) {
+      for (let car of objs.cars.vehicles) {
+        if(car["_id"] == carId) {
+          car["fav"] = !car["fav"];
+        }  
+      } 
+    }
+  }  
+
+  // function on click to add to cart
+  addToCart($event, carId){    
+    for (let objs of this.carDataAllInOne) {
+      for (let car of objs.cars.vehicles) {
+        if(car["_id"] == carId) {
+          car["addedToCart"] = !car["addedToCart"];
+        }  
+      } 
+    }   
+  } 
 }
--- OldCar/src/app/post.service.ts
+++ OldCar/src/app/post.service.ts
@@ -1,16 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
+   
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
   private url = 'https://api.jsonbin.io/b/5ebe673947a2266b1478d892';
-
+    
   constructor(private httpClient: HttpClient) { }
-
+   
   getPosts(){
     return this.httpClient.get(this.url);
   }
-
+    
 }
